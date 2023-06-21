5e12088834e8a630b0ef4f464beef6bea84853a0
Federico6419 <102321945+Federico6419@users.noreply.github.com>
2023-06-21 12:28:42 +0200

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
 DEVICE ="cuda" if torch.cuda.is_available() else "cpu"
-TRAIN_DIR = "tree/main/Datasets/trainWinterr"
+TRAIN_DIR = "tree/main/Datasets/trainWinter"
 TEST_DIR = "tree/main/Datasets/testWinter"
 TRAIN_DIR_ALT="tree/main/Datasets/trainSummer"
 TEST_DIR_ALT ="tree/main/Datasets/testSummer"
