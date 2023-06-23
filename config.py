ffdbcd5b60253999f4c601911bcb649d5ebb040c
Federico6419 <102321945+Federico6419@users.noreply.github.com>
2023-06-23 16:24:04 +0200

    Update config.py

--- config.py
+++ config.py
@@ -11,16 +11,16 @@ GAMMA_CYCLE = 0.1
 LAMBDA_CYCLE = 10
 LAMBDA_IDENTITY = 5 # 0.5*lambda_cycle
 NUM_EPOCHS = 10
-LOAD_MODEL = False
+LOAD_MODEL = True
 SAVE_MODEL = True
 TRAIN_MODEL = True
 TRANSFORMATION = "HorseToZebra"                          
 BETTER= False
 
-CHECKPOINT_GEN_A = "../drive/MyDrive/Checkpoints/HorseToZebra20/gen_a.pth.tar"     
-CHECKPOINT_GEN_B= "../drive/MyDrive/Checkpoints/HorseToZebra20/gen_b.pth.tar"
-CHECKPOINT_DISC_A = "../drive/MyDrive/Checkpoints/HorseToZebra20/disc_a.pth.tar"
-CHECKPOINT_DISC_B = "../drive/MyDrive/Checkpoints/HorseToZebra20/disc_b.pth.tar"
+CHECKPOINT_GEN_A = "../drive/MyDrive/Checkpoints/HorseToZebra10/gen_a.pth.tar"     
+CHECKPOINT_GEN_B= "../drive/MyDrive/Checkpoints/HorseToZebra10/gen_b.pth.tar"
+CHECKPOINT_DISC_A = "../drive/MyDrive/Checkpoints/HorseToZebra10/disc_a.pth.tar"
+CHECKPOINT_DISC_B = "../drive/MyDrive/Checkpoints/HorseToZebra10/disc_b.pth.tar"
 
 NEW_CHECKPOINT_GEN_A = "Checkpoints/gen_a.pth.tar"     
 NEW_CHECKPOINT_GEN_B= "Checkpoints/gen_b.pth.tar"
