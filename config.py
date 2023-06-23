83ee792e490265126bbd471fe562330189396cbc
Federico6419 <102321945+Federico6419@users.noreply.github.com>
2023-06-23 12:47:28 +0200

    Update config.py

--- config.py
+++ config.py
@@ -17,10 +17,10 @@ TRAIN_MODEL = True
 TRANSFORMATION = "HorseToZebra"                          
 BETTER= False
 
-CHECKPOINT_GEN_A = "drive/MyDrive/Checkpoints/HorseToZebra20/gen_a.pth.tar"     
-CHECKPOINT_GEN_B= "drive/MyDrive/Checkpoints/HorseToZebra20/gen_b.pth.tar"
-CHECKPOINT_DISC_A = "drive/MyDrive/Checkpoints/HorseToZebra20/disc_a.pth.tar"
-CHECKPOINT_DISC_B = "drive/MyDrive/Checkpoints/HorseToZebra20/disc_b.pth.tar"
+CHECKPOINT_GEN_A = "../drive/MyDrive/Checkpoints/HorseToZebra20/gen_a.pth.tar"     
+CHECKPOINT_GEN_B= "../drive/MyDrive/Checkpoints/HorseToZebra20/gen_b.pth.tar"
+CHECKPOINT_DISC_A = "../drive/MyDrive/Checkpoints/HorseToZebra20/disc_a.pth.tar"
+CHECKPOINT_DISC_B = "../drive/MyDrive/Checkpoints/HorseToZebra20/disc_b.pth.tar"
 
 NEW_CHECKPOINT_GEN_A = "Checkpoints/gen_a.pth.tar"     
 NEW_CHECKPOINT_GEN_B= "Checkpoints/gen_b.pth.tar"
