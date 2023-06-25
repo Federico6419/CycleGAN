577858f6baa2d13efb111771755f61a4785343ca
Federico6419 <102321945+Federico6419@users.noreply.github.com>
2023-06-25 17:35:15 +0200

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ NUM_EPOCHS = 5
 LOAD_MODEL = False
 SAVE_MODEL = True
 TRAIN_MODEL = True
-TRANSFORMATION = "HorseToZebra"                          
+TRANSFORMATION = "MonetToPhoto"                          
 BETTER= False
 
 CHECKPOINT_GEN_A = "../drive/MyDrive/Checkpoints/HorseToZebra0/gen_a.pth.tar"     
@@ -22,10 +22,10 @@ CHECKPOINT_GEN_B= "../drive/MyDrive/Checkpoints/HorseToZebra0/gen_b.pth.tar"
 CHECKPOINT_DISC_A = "../drive/MyDrive/Checkpoints/HorseToZebra0/disc_a.pth.tar"
 CHECKPOINT_DISC_B = "../drive/MyDrive/Checkpoints/HorseToZebra0/disc_b.pth.tar"
 
-NEW_CHECKPOINT_GEN_A = "../drive/MyDrive/Checkpoints/HorseToZebra10/gen_a.pth.tar"     
-NEW_CHECKPOINT_GEN_B= "../drive/MyDrive/Checkpoints/HorseToZebra10/gen_b.pth.tar"
-NEW_CHECKPOINT_DISC_A = "../drive/MyDrive/Checkpoints/HorseToZebra10/disc_a.pth.tar"
-NEW_CHECKPOINT_DISC_B = "../drive/MyDrive/Checkpoints/HorseToZebra10/disc_b.pth.tar"
+NEW_CHECKPOINT_GEN_A = "../drive/MyDrive/Checkpoints/MonetToPhoto5/gen_a.pth.tar"     
+NEW_CHECKPOINT_GEN_B= "../drive/MyDrive/Checkpoints/MonetToPhoto5/gen_b.pth.tar"
+NEW_CHECKPOINT_DISC_A = "../drive/MyDrive/Checkpoints/MonetToPhoto5/disc_a.pth.tar"
+NEW_CHECKPOINT_DISC_B = "../drive/MyDrive/Checkpoints/MonetToPhoto5/disc_b.pth.tar"
 
 transforms = A.Compose(
     [
