ae683b6720f1137f4798b5c48cbd9f6bd312115e
Federico6419 <102321945+Federico6419@users.noreply.github.com>
2023-06-25 23:35:34 +0200

    Update config.py

--- config.py
+++ config.py
@@ -11,21 +11,21 @@ GAMMA_CYCLE = 0.1
 LAMBDA_CYCLE = 10
 LAMBDA_IDENTITY = 0 # 0.5*lambda_cycle
 NUM_EPOCHS = 10
-LOAD_MODEL = False
+LOAD_MODEL = True
 SAVE_MODEL = True
 TRAIN_MODEL = True
 TRANSFORMATION = "HorseToZebra"                          
 BETTER= False
 
-CHECKPOINT_GEN_A = "../drive/MyDrive/Checkpoints/HorseToZebra5/gen_a.pth.tar"     
-CHECKPOINT_GEN_B= "../drive/MyDrive/Checkpoints/HorseToZebra5/gen_b.pth.tar"
-CHECKPOINT_DISC_A = "../drive/MyDrive/Checkpoints/HorseToZebra5/disc_a.pth.tar"
-CHECKPOINT_DISC_B = "../drive/MyDrive/Checkpoints/HorseToZebra5/disc_b.pth.tar"
+CHECKPOINT_GEN_A = "../drive/MyDrive/Checkpoints/HorseToZebraIdentity7/gen_a.pth.tar"     
+CHECKPOINT_GEN_B= "../drive/MyDrive/Checkpoints/HorseToZebraIdentity7/gen_b.pth.tar"
+CHECKPOINT_DISC_A = "../drive/MyDrive/Checkpoints/HorseToZebraIdentity7/disc_a.pth.tar"
+CHECKPOINT_DISC_B = "../drive/MyDrive/Checkpoints/HorseToZebraIdentity7/disc_b.pth.tar"
 
-NEW_CHECKPOINT_GEN_A = "../drive/MyDrive/Checkpoints/HorseToZebraIdentity10/gen_a.pth.tar"     
-NEW_CHECKPOINT_GEN_B= "../drive/MyDrive/Checkpoints/HorseToZebraIdentity10/gen_b.pth.tar"
-NEW_CHECKPOINT_DISC_A = "../drive/MyDrive/Checkpoints/HorseToZebraIdentity10/disc_a.pth.tar"
-NEW_CHECKPOINT_DISC_B = "../drive/MyDrive/Checkpoints/HorseToZebraIdentity10/disc_b.pth.tar"
+NEW_CHECKPOINT_GEN_A = "../drive/MyDrive/Checkpoints/HorseToZebraIdentity17/gen_a.pth.tar"     
+NEW_CHECKPOINT_GEN_B= "../drive/MyDrive/Checkpoints/HorseToZebraIdentity17/gen_b.pth.tar"
+NEW_CHECKPOINT_DISC_A = "../drive/MyDrive/Checkpoints/HorseToZebraIdentity17/disc_a.pth.tar"
+NEW_CHECKPOINT_DISC_B = "../drive/MyDrive/Checkpoints/HorseToZebraIdentity17/disc_b.pth.tar"
 
 transforms = A.Compose(
     [
