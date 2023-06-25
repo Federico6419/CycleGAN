eaa4300bb72a1a69c9b9d6673467e8610820cc93
Federico6419 <102321945+Federico6419@users.noreply.github.com>
2023-06-25 16:41:52 +0200

    Update generator.py

--- generator.py
+++ generator.py
@@ -2,9 +2,9 @@ import torch
 import torch.nn as nn
 
  #Function that initializes weights from aGaussian distribution N(0, 0.02)
-        def init_weights(m):
-            if(type(m) == nn.Conv2d or type(m) == nn.ConvTranspose2d):
-                torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
+ def init_weights(m):
+     if(type(m) == nn.Conv2d or type(m) == nn.ConvTranspose2d):
+         torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
                 
 #Define the Residual Block structure, composed by a convolutional layer and an instance normalization (only the first use an activation function)
 class ResidualBlock(nn.Module):
@@ -16,7 +16,7 @@ class ResidualBlock(nn.Module):
             nn.ReLU(inplace=True),
             nn.Conv2d(channels, channels, kernel_size=3, padding_mode="reflect", padding=1),
             nn.InstanceNorm2d(channels)
-        )
+        ).apply(init_weights)
   
     def forward(self, x):
         return x + self.block(x)
@@ -30,17 +30,17 @@ class Generator(nn.Module):
             nn.Conv2d(img_channels, 64, kernel_size=7, stride=1, padding=3, padding_mode="reflect"),
             nn.InstanceNorm2d(64),
             nn.ReLU(inplace=True),
-        )
+        ).apply(init_weights)
         self.conv1 = nn.Sequential(
             nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1, padding_mode="reflect"),
             nn.InstanceNorm2d(128),
             nn.ReLU(inplace=True)   
-        )
+        ).apply(init_weights)
         self.conv2 = nn.Sequential(
             nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1, padding_mode="reflect"),
             nn.InstanceNorm2d(256),
             nn.ReLU(inplace=True)   
-        )
+        ).apply(init_weights)
 
         ############################   RESIDUAL BLOCKS    ##################################
         self.res_blocks = nn.Sequential(
@@ -52,13 +52,13 @@ class Generator(nn.Module):
             nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1, output_padding=1),
             nn.InstanceNorm2d(128),
             nn.ReLU(inplace=True)
-        )
+        ).apply(init_weights)
         
         self.tran2 = nn.Sequential(
             nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, output_padding=1),
             nn.InstanceNorm2d(64),
             nn.ReLU(inplace=True)
-        )
+        ).apply(init_weights)
         
         ####################### LAST LAYER ############## 
         self.last = nn.Conv2d(64, img_channels, kernel_size=7, stride=1, padding=3, padding_mode="reflect")
